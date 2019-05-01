af82766fed3adddde01ab8324837e292e026b3dd
neilpirch <neil.pirch@gmail.com>
2019-05-01 16:48:15 -0500

    added pokedex entry partial view

---
+++ PokemonIndex/PokemonIndex/Models/PokedexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PokemonIndex.Models
+{
+    public class PokedexViewModel
+    {
+        public Pokemon  Pokemon { get; set; }
+        public List<PokemonType> PokemonTypes { get; set; }
+        public Pokemon EvolvesFrom { get; set; }
+        public List<Pokemon> EvolvesTo { get; set; }
+    }
+}
