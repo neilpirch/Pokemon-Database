a8c92cfbfacc9937b46912b8fb2fb489e5b3ea0f
neilpirch <neil.pirch@gmail.com>
2019-05-03 17:04:17 -0500

    Pokedex search works, returns all data except "owned by"

--- PokemonIndex/PokemonIndex/Controllers/PokedexController.cs
+++ PokemonIndex/PokemonIndex/Controllers/PokedexController.cs
@@ -1,7 +1,9 @@
 ﻿using PokemonIndex.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,7 +13,7 @@ namespace PokemonIndex.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: PokedexViewModel
-        
+
         public ActionResult Index(string id)
         {
             var pokemon = from p in db.Pokemons
@@ -19,14 +21,67 @@ namespace PokemonIndex.Controllers
 
             if (!String.IsNullOrEmpty(id))
             {
-                pokemon = pokemon.Where(s => s.Name.Contains(id));
-                return View("Result",pokemon.ToList());
+                return RedirectToAction("IndexResult", new { id = id });
             }
-
             return View();
-
         }
 
+        public async Task<ActionResult> IndexResult(string id)
+        {
+            var searchResults = from p in db.Pokemons
+                          select p;
+            var pokemonId = 1;
+            var pokemonTypes = db.PokemonTypes;
+            var pokemons = db.Pokemons;
+            var evolutions = db.Evolutions;
 
+            List<PokedexViewModel> indexList = new List<PokedexViewModel>();
+
+            if (!String.IsNullOrEmpty(id))
+            {
+                searchResults = pokemons.Where(s => s.Name.Contains(id));
+                List<Pokemon> searchResultList = await searchResults.ToListAsync();
+                foreach(Pokemon item in searchResultList)
+                {
+                    pokemonId = item.PokemonId;
+                    var types = pokemonTypes.Where(t => (int)(t.PokemonId) == pokemonId);
+                    List<PokemonType> typesList = await types.ToListAsync();
+
+                    var evolvesFromId = evolutions
+                        .Where(e => e.EvolveToId == pokemonId)
+                        .Select(e => e.EvolveFromId)
+                        .FirstOrDefault();
+
+                    var evolvesFrom = pokemons
+                        .Where(p => p.PokemonId == evolvesFromId)
+                        .FirstOrDefault();
+
+                    var evolvesToEvolutions = evolutions
+                        .Where(e => e.EvolveFromId == pokemonId)
+                        .Select(e => e.EvolveToId);
+
+                    List<int> evolvesToIdList = await evolvesToEvolutions.ToListAsync();
+
+                    List<Pokemon> evolvesTo = new List<Pokemon>();
+
+                    foreach (int evo in evolvesToIdList)
+                    {
+                        evolvesTo.Add(pokemons
+                        .Where(p => p.PokemonId == evo)
+                        .FirstOrDefault());
+                    }
+
+                    var viewModel = new PokedexViewModel
+                    {
+                        Pokemon = item,
+                        PokemonTypes = typesList,
+                        EvolvesFrom = evolvesFrom,
+                        EvolvesTo = evolvesTo
+                    };
+                    indexList.Add(viewModel);
+                }
+            }
+            return View("Result", indexList);
+        }
     }
 }
